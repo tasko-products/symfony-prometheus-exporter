059ce677d11c21de783b36c47f7695c41aac6fa0
Lukas Rotermund <lukas.rotermund@live.de>
2022-04-12 10:15:48 +0200

    test red due to missing messenger error counter

--- tests/UnitTest/MessengerEventMiddlewareTest.php
+++ tests/UnitTest/MessengerEventMiddlewareTest.php
@@ -24,6 +24,7 @@ class MessengerEventMiddlewareTest extends TestCase
 
     private const BUS_NAME = 'message_bus';
     private const METRIC_NAME = 'messenger';
+    private const ERROR_METRIC_NAME = self::METRIC_NAME . '_error';
 
     protected function setUp(): void
     {
@@ -32,8 +33,10 @@ class MessengerEventMiddlewareTest extends TestCase
 
     public function testCollectFooBarMessageSuccessfully(): void
     {
+        $givenRouting = [FooBarMessage::class => [new FooBarMessageHandler()]];
+
         $messageBus = MessageBusFactory::create(
-            [FooBarMessage::class => [new FooBarMessageHandler()]],
+            $givenRouting,
             new AddBusNameStampMiddleware(self::BUS_NAME),
             new MessengerEventMiddleware($this->registry, self::METRIC_NAME)
         );
@@ -43,5 +46,33 @@ class MessengerEventMiddlewareTest extends TestCase
         $counter = $this->registry->getCounter(self::BUS_NAME, self::METRIC_NAME);
 
         $this->assertEquals(self::BUS_NAME . '_' . self::METRIC_NAME, $counter->getName());
+        $this->assertEquals(['message', 'label'], $counter->getLabelNames());
+
+        $expectedMetricCounter = 1;
+        $metrics = $this->registry->getMetricFamilySamples();
+        $samples = $metrics[0]->getSamples();
+
+        $this->assertEquals($expectedMetricCounter, $samples[0]->getValue());
+        $this->assertEquals([FooBarMessage::class, 'FooBarMessage'], $samples[0]->getLabelValues());
+    }
+
+    public function testCollectMessengerExceptionsSuccessfully(): void
+    {
+        $givenRouting = [FooBarMessage::class => [new FooBarMessageHandlerWithException()]];
+
+        $messageBus = MessageBusFactory::create(
+            $givenRouting,
+            new AddBusNameStampMiddleware(self::BUS_NAME),
+            new MessengerEventMiddleware($this->registry, self::METRIC_NAME)
+        );
+
+        try {
+            $messageBus->dispatch(new FooBarMessage());
+        } catch (\Throwable $exception) {
+        }
+
+        $counter = $this->registry->getCounter(self::BUS_NAME, self::ERROR_METRIC_NAME);
+
+        $this->assertEquals(self::BUS_NAME . '_' . self::ERROR_METRIC_NAME, $counter->getName());
     }
 }
