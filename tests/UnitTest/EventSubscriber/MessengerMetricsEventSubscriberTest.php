b77211ad23472a587fc041a418cc5e05da3fbe00
Lukas Rotermund <lukas.rotermund@live.de>
2022-04-14 13:14:24 +0200

    test red due to missing WorkerStartedEvent subscription

---
+++ tests/UnitTest/EventSubscriber/MessengerMetricsEventSubscriberTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @link            https://tasko.de/ tasko Products GmbH
+ * @copyright       (c) tasko Products GmbH 2022
+ * @license         tbd
+ * @author          Lukas Rotermund <lukas.rotermund@tasko.de>
+ * @version         1.0.0
+ */
+
+declare(strict_types=1);
+
+namespace TaskoProducts\SymfonyPrometheusExporterBundle\Tests\UnitTest\EventSubscriber;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Messenger\Event\WorkerStartedEvent;
+use TaskoProducts\SymfonyPrometheusExporterBundle\EventSubscriber\MessengerMetricsEventSubscriber;
+
+class MessengerMetricsEventSubscriberTest extends TestCase
+{
+    public function testWorkerStartedEventIsSubscribedByMessengerMetricsEventSubscriber(): void
+    {
+        $this->assertContains(
+            WorkerStartedEvent::class,
+            MessengerMetricsEventSubscriber::getSubscribedEvents()
+        );
+    }
+}
