44741a5df8312b1e4dc82ca348ee18f2ff6bbfd4
Lukas Rotermund <lukas.rotermund@live.de>
2022-04-21 08:53:52 +0200

    prepared basic bundle extension and a services.yaml

---
+++ src/DependencyInjection/TaskoProductsSymfonyPrometheusExporterExtension.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @link            https://tasko.de/ tasko Products GmbH
+ * @copyright       (c) tasko Products GmbH 2022
+ * @license         tbd
+ * @author          Lukas Rotermund <lukas.rotermund@tasko.de>
+ * @version         1.0.0
+ */
+
+declare(strict_types=1);
+
+namespace TaskoProducts\SymfonyPrometheusExporterBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class TaskoProductsSymfonyPrometheusExporterExtension extends Extension
+{
+    /**
+     * @inheritDoc
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
+    }
+}
