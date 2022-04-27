1b28aee18cfeba64a9f3a6ffc37afc23c5416ffc
Lukas Rotermund <lukas.rotermund@live.de>
2022-04-27 13:12:14 +0200

    test red due to missing Help data type

--- src/Controller/OpenMetricsController.php
+++ src/Controller/OpenMetricsController.php
@@ -11,12 +11,17 @@ declare(strict_types=1);
 
 namespace TaskoProducts\SymfonyPrometheusExporterBundle\Controller;
 
+use Prometheus\RegistryInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OpenMetricsController extends AbstractController
 {
+    public function __construct(private RegistryInterface $registry)
+    {
+    }
+
     #[Route('/metrics', name: 'open_metrics', methods: ['GET'])]
     public function metrics(): Response
     {
--- tests/UnitTest/Controller/OpenMetricsControllerTest.php
+++ tests/UnitTest/Controller/OpenMetricsControllerTest.php
@@ -12,16 +12,73 @@ declare(strict_types=1);
 namespace TaskoProducts\SymfonyPrometheusExporterBundle\Tests\UnitTest\Controller;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
+use Symfony\Component\Messenger\Event\WorkerStartedEvent;
+use Symfony\Component\Messenger\Worker;
+use Symfony\Component\Messenger\WorkerMetadata;
+use TaskoProducts\SymfonyPrometheusExporterBundle\Configuration\ConfigurationProvider;
 use TaskoProducts\SymfonyPrometheusExporterBundle\Controller\OpenMetricsController;
+use TaskoProducts\SymfonyPrometheusExporterBundle\EventSubscriber\ActiveWorkersMetricEventSubscriber;
+use TaskoProducts\SymfonyPrometheusExporterBundle\Tests\Factory\PrometheusCollectorRegistryFactory;
 
 class OpenMetricsControllerTest extends TestCase
 {
     public function testGetOpenMetricsForActiveWorkers(): void
     {
-        $controller = new OpenMetricsController();
+        $registry = PrometheusCollectorRegistryFactory::create();
+        $worker = $this->getMockBuilder(Worker::class)
+                       ->disableOriginalConstructor()
+                       ->getMock();
+
+        $worker->expects($this->any())
+               ->method('getMetadata')
+               ->willReturn(new WorkerMetadata([
+                   'transportNames' => ['transport', 'prio_transport'],
+                   'queueNames' => [
+                    'foobar_worker_queue',
+                    'priority_foobar_worker_queue',
+                   ]
+               ]));
+
+        $subscriber = new ActiveWorkersMetricEventSubscriber(
+            $registry,
+            new ConfigurationProvider(
+                new ParameterBag(
+                    [
+                        'prometheus_metrics.event_subscribers' => [
+                            'active_workers' => [
+                                'enabled' => true,
+                                'namespace' => 'test_namespace',
+                                'metric_name' => 'test_metric',
+                                'help_text' => 'test help text',
+                                'labels' => [
+                                    'queue_names' => 'test_queue_names',
+                                    'transport_names' => 'test_transport_names',
+                                ],
+                            ],
+                        ],
+                    ],
+                ),
+            ),
+        );
+
+        $subscriber->onWorkerStarted(new WorkerStartedEvent($worker));
+
+        $controller = new OpenMetricsController($registry);
 
         $metricsResponse = $controller->metrics();
 
         $this->assertEquals(200, $metricsResponse->getStatusCode());
+
+        /**
+         * @var string $content
+         */
+        $content = $metricsResponse->getContent();
+
+        $this->assertIsString($content);
+        $this->assertStringContainsString(
+            '# HELP test_namespace_test_metric test help text',
+            $content,
+        );
     }
 }
